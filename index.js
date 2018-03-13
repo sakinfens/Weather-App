0096f73df5b79ad44fb59ccd38219c91d408f11c
Cryptolemming <aliayoub22@gmail.com>
2018-03-13 12:38:43 -0400

    refactor city and weather condition logic

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let WEATHER_DATA = []; // 
+let WEATHER_DATA = []; //
+let weatherState = 'main-condition';
+let city = 'Bronx';
+
 const CONDITIONS = {
   main: {
     'Clear sky': 'http://openweathermap.org/img/w/01d.png',
@@ -17,38 +20,55 @@ const CONDITIONS = {
     ]
 };
 
-$('.location-form').submit(event => {
-  event.preventDefault();
-  const area = $(event.currentTarget).find('#location');
-  let city = area.val();
-  console.log('city from submission: ' + city);
-  area.val(" ");
-  weatherInformation(city);
-});
+function handleCitySubmit(){
+    $('.location-form').submit(event => {
+      event.preventDefault();
+      const area = $(event.currentTarget).find('#location');
+      city = area.val();
+      area.val(" ");
+      weatherState = 'main-condition';
+      fetchWeatherData();
+    });
+}
 
 // google map api is going to render the map to .map
 // the location will be provided from input
-// we'll use that input to pass to map api to focus on the area 
+// we'll use that input to pass to map api to focus on the area
 
 // weather api
-function weatherInformation(city){
-  $('.weather-text').html(`<p></p>`);
-  $('.weather-image').html(`<div></div>`);
-  console.log('city from weatherInformation: ' + city);
+function fetchWeatherData(){
   $.ajax({
     url : `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=3c2ae1668b32134ba5c910ca0375caaa`,
     type: "GET",
     dataType: "jsonp",
     success: function(data) {
       WEATHER_DATA = data;
-      console.log('weaterdata from weatherInformation: ' + WEATHER_DATA);
+      console.log('weatherdata from fetchWeatherData: ' + WEATHER_DATA);
       initMap();
+      renderWeatherCondition();
     }
   });
 }
 
+function renderWeatherCondition() {
+    switch (weatherState) {
+      case 'main-condition':
+        let condition = WEATHER_DATA.weather[0].main;
+        $('.weather-image').html(`<img src=${CONDITIONS.main[condition]}>`);
+        $('.weather-text').html(`<p>${condition}</p>`);
+        break;
+      case 'wind-speed':
+        renderWindCondition();
+        break;
+      case 'temp-measure':
+        renderTemperatureCondition();
+        break;
+      default:
+        break;
+    }
+}
+
 function initMap() {
-  console.log('weatherdata from initMap: ' + JSON.stringify(WEATHER_DATA));
   let latNum = WEATHER_DATA.coord.lat || 40;
   let lngNum = WEATHER_DATA.coord.lon || 73;
   let coordinates = {
@@ -66,73 +86,57 @@ function initMap() {
 }
 
 // Attach a delegated event handler
-$('.weather-options').on( "click", "button", function( event ) {
-    event.preventDefault();
-    switch ($(this).attr('class')) {
-      case 'main-condition': 
-        let condition = WEATHER_DATA.weather[0].main;
-        $('.weather-image').html(`<img src=${CONDITIONS.main[condition]}>`);
-        $('.weather-text').html(`<p>${condition}</p>`);
-        break;
-      case 'wind-speed':
-        windConditions();
-        break;
-      case 'temp-measure':
-        temperatureConditions();
-        let temp = WEATHER_DATA.main.temp;
-        console.log(temp);
-        break;
-      default:
-        break;
-    }
-});
+function handleWeatherClick() {
+    $('.weather-options').on( "click", "button", function( event ) {
+        event.preventDefault();
+        weatherState = $(this).attr('class');
+        renderWeatherCondition();
+    });
+}
 
-// their weather selection from input will be passed to the 
-// weather api 
+// their weather selection from input will be passed to the
+// weather api
 let mainCond = 'thunderstorm';
 
 // these should all be moved to a CONDITIONS key
-function windConditions(){
+function renderWindCondition(){
   let windspeed = WEATHER_DATA.wind.speed;
   if(windspeed >= .1 && windspeed <= 5){
     $('.weather-image').html(`<img ${CONDITIONS.wind[2]}>`);
     $('.weather-text').html(`<p>Light Breeze ${windspeed}m/s</p>`);
-    console.log('yess');
   }else if(windspeed >= 5.01 && windspeed <= 16.99){
     $('.weather-image').html(`<img ${CONDITIONS.wind[1]}`);
     $('.weather-text').html(`<p>Moderate Breeze ${windspeed}m/s</p>`);
-    console.log('wind');
   }else if(windspeed >= 17 && windspeed <= 27){
     $('.weather-image').html(`<img ${CONDITIONS.wind[0]}>`);
     $('.weather-text').html(`<p>Strong Gale ${windspeed}m/s</p>`);
-    console.log("High Wind Speed");
   }
 }
 
-function temperatureConditions(){
+function renderTemperatureCondition(){
   let temp = WEATHER_DATA.main.temp;
-  console.log('yes');
   if(temp <= 0){
     $('.weather-image').html(`<img ${CONDITIONS.temperature[0]}>`);
     $('.weather-text').html(`<p>${temp}°C</p>`);
-    console.log('Cold');
   }
   else if(temp >= .5 && temp <=10){
     $('.weather-image').html(`<img ${CONDITIONS.temperature[1]}>`);
     $('.weather-text').html(`<p>${temp}°C</p>`);
-    console.log('Sweater Weather');
   }
   else if(temp >= 10.55 && temp <=21){
     $('.weather-image').html(`<img ${CONDITIONS.temperature[2]}>`);
     $('.weather-text').html(`<p>${temp}°C</p>`);
-    console.log('Warm');
   }
   else if(temp >= 21.1 && temp <=27){
     $('.weather-image').html(`<img ${CONDITIONS.temperature[3]}>`);
     $('.weather-text').html(`<p>${temp}°C</p>`);
-    console.log('Nice Hot');
   }
-
 }
 
-$(weatherInformation('Bronx'));
+function start() {
+    fetchWeatherData();
+    handleCitySubmit();
+    handleWeatherClick();
+}
+
+$(start());
